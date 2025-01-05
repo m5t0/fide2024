8fb11c82fffb9f8342cba19231f8e8bb56f4e66a
moto <mo_to1015@yahoo.co.jp>
2025-01-06 00:49:24 +0900

    Revert "stockfish16の評価関数のパラメーターを使ってみる"
    This reverts commit 9aae29532e6ad2d1b4f4290d8fa88f6b7e4329b4.

--- fide2024/material.cpp
+++ fide2024/material.cpp
@@ -1,6 +1,8 @@
 /*
   Stockfish, a UCI chess playing engine derived from Glaurung 2.1
-  Copyright (C) 2004-2023 The Stockfish developers (see AUTHORS file)
+  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
+  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2015-2020 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
 
   Stockfish is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -25,248 +27,193 @@
 using namespace std;
 
 namespace {
-#define S(mg, eg) make_score(mg, eg)
 
-    // Polynomial material imbalance parameters
+  // Polynomial material imbalance parameters
 
-    //constexpr int QuadraticOurs[][PIECE_TYPE_NB] = {
-    //    //            OUR PIECES
-    //    // pair pawn knight bishop rook queen
-    //    {1438                               }, // Bishop pair
-    //    {  40,   38                         }, // Pawn
-    //    {  32,  255, -62                    }, // Knight      OUR PIECES
-    //    {   0,  104,   4,    0              }, // Bishop
-    //    { -26,   -2,  47,   105,  -208      }, // Rook
-    //    {-189,   24, 117,   133,  -134, -6  }  // Queen
-    //};
+  constexpr int QuadraticOurs[][PIECE_TYPE_NB] = {
+    //            OUR PIECES
+    // pair pawn knight bishop rook queen
+    {1438                               }, // Bishop pair
+    {  40,   38                         }, // Pawn
+    {  32,  255, -62                    }, // Knight      OUR PIECES
+    {   0,  104,   4,    0              }, // Bishop
+    { -26,   -2,  47,   105,  -208      }, // Rook
+    {-189,   24, 117,   133,  -134, -6  }  // Queen
+  };
 
+  constexpr int QuadraticTheirs[][PIECE_TYPE_NB] = {
+    //           THEIR PIECES
+    // pair pawn knight bishop rook queen
+    {                                   }, // Bishop pair
+    {  36,                              }, // Pawn
+    {   9,   63,                        }, // Knight      OUR PIECES
+    {  59,   65,  42,                   }, // Bishop
+    {  46,   39,  24,   -24,            }, // Rook
+    {  97,  100, -42,   137,  268,      }  // Queen
+  };
 
-    // One Score parameter for each pair (our piece, another of our pieces)
-    constexpr Score QuadraticOurs[][PIECE_TYPE_NB] = {
-        // OUR PIECE 2
-        // bishop pair    pawn         knight       bishop       rook           queen
-        {S(1420, 1456)                                                                   }, // Bishop pair
-        {S(77,   4),    S(37,  39)                                                       }, // Pawn
-        {S(29,   36),   S(286, 224), S(-56, -69)                                         }, // Knight      OUR PIECE 1
-        {S(0,    0),    S(141, 210), S(-4,  13),    S(0,   0)                              }, // Bishop
-        {S(-24,  -29),  S(-6,   2),  S(115,  96),  S(107, 93), S(-211, -209)             }, // Rook
-        {S(-189, -190), S(36,  13),  S(120, 114), S(161, 105), S(-126, -142), S(5,-17)   }  // Queen
-    };
+  // Endgame evaluation and scaling functions are accessed directly and not through
+  // the function maps because they correspond to more than one material hash key.
+  Endgame<KXK>    EvaluateKXK[] = { Endgame<KXK>(WHITE),    Endgame<KXK>(BLACK) };
 
-    //// One Score parameter for each pair (our piece, another of our pieces)
-    //constexpr Score QuadraticOurs[][PIECE_TYPE_NB] = {
-    //    // OUR PIECE 2
-    //    // bishop pair    pawn         knight       bishop       rook           queen
-    //    {S(1419, 1455)                                                                  }, // Bishop pair
-    //    {S(101,   28), S(37,  39)                                                     }, // Pawn
-    //    {S(57,   64), S(249, 187), S(-49, -62)                                        }, // Knight      OUR PIECE 1
-    //    {S(0,    0), S(118, 137), S(10,  27), S(0,   0)                           }, // Bishop
-    //    {S(-63,  -68), S(-5,   3), S(100,  81), S(132, 118), S(-246, -244)            }, // Rook
-    //    {S(-210, -211), S(37,  14), S(147, 141), S(161, 105), S(-158, -174), S(-9,-31) }  // Queen
-    //};
+  Endgame<KBPsK>  ScaleKBPsK[]  = { Endgame<KBPsK>(WHITE),  Endgame<KBPsK>(BLACK) };
+  Endgame<KQKRPs> ScaleKQKRPs[] = { Endgame<KQKRPs>(WHITE), Endgame<KQKRPs>(BLACK) };
+  Endgame<KPsK>   ScaleKPsK[]   = { Endgame<KPsK>(WHITE),   Endgame<KPsK>(BLACK) };
+  Endgame<KPKP>   ScaleKPKP[]   = { Endgame<KPKP>(WHITE),   Endgame<KPKP>(BLACK) };
 
-    //constexpr int QuadraticTheirs[][PIECE_TYPE_NB] = {
-//    //           THEIR PIECES
-//    // pair pawn knight bishop rook queen
-//    {                                   }, // Bishop pair
-//    {  36,                              }, // Pawn
-//    {   9,   63,                        }, // Knight      OUR PIECES
-//    {  59,   65,  42,                   }, // Bishop
-//    {  46,   39,  24,   -24,            }, // Rook
-//    {  97,  100, -42,   137,  268,      }  // Queen
-//};
+  // Helper used to detect a given material distribution
+  bool is_KXK(const Position& pos, Color us) {
+    return  !more_than_one(pos.pieces(~us))
+          && pos.non_pawn_material(us) >= RookValueMg;
+  }
 
-    // One Score parameter for each pair (our piece, their piece)
-    constexpr Score QuadraticTheirs[][PIECE_TYPE_NB] = {
-        // THEIR PIECE
-        // bishop pair   pawn         knight       bishop       rook         queen
-        {                                                                               }, // Bishop pair
-        {S(28,  25)                                                                     }, // Pawn
-        {S(23,  -5), S(74,  62)                                                          }, // Knight      OUR PIECE
-        {S(79,  39), S(73,  58), S(65,  20)                                             }, // Bishop
-        {S(42,  51), S(16,  32), S(24,  25), S(-39,  -19)                               }, // Rook
-        {S(99,  95), S(69, 131), S(-90, -123), S(94, 174), S(294, 243)                  }  // Queen
-    };
+  bool is_KBPsK(const Position& pos, Color us) {
+    return   pos.non_pawn_material(us) == BishopValueMg
+          && pos.count<PAWN  >(us) >= 1;
+  }
 
-    //// One Score parameter for each pair (our piece, their piece)
-    //constexpr Score QuadraticTheirs[][PIECE_TYPE_NB] = {
-    //    // THEIR PIECE
-    //    // bishop pair   pawn         knight       bishop       rook         queen
-    //    {                                                                               }, // Bishop pair
-    //    {S(33,  30)                                                                   }, // Pawn
-    //    {S(46,  18), S(106,  84)                                                      }, // Knight      OUR PIECE
-    //    {S(75,  35), S(59,  44), S(60,  15)                                         }, // Bishop
-    //    {S(26,  35), S(6,  22), S(38,  39), S(-12,  -2)                            }, // Rook
-    //    {S(97,  93), S(100, 163), S(-58, -91), S(112, 192), S(276, 225)               }  // Queen
-    //};
+  bool is_KQKRPs(const Position& pos, Color us) {
+    return  !pos.count<PAWN>(us)
+          && pos.non_pawn_material(us) == QueenValueMg
+          && pos.count<ROOK>(~us) == 1
+          && pos.count<PAWN>(~us) >= 1;
+  }
 
-#undef S
+  /// imbalance() calculates the imbalance by comparing the piece count of each
+  /// piece type for both colors.
+  template<Color Us>
+  int imbalance(const int pieceCount[][PIECE_TYPE_NB]) {
 
-    // Endgame evaluation and scaling functions are accessed directly and not through
-    // the function maps because they correspond to more than one material hash key.
-    Endgame<KXK>    EvaluateKXK[] = { Endgame<KXK>(WHITE),    Endgame<KXK>(BLACK) };
+    constexpr Color Them = ~Us;
 
-    Endgame<KBPsK>  ScaleKBPsK[] = { Endgame<KBPsK>(WHITE),  Endgame<KBPsK>(BLACK) };
-    Endgame<KQKRPs> ScaleKQKRPs[] = { Endgame<KQKRPs>(WHITE), Endgame<KQKRPs>(BLACK) };
-    Endgame<KPsK>   ScaleKPsK[] = { Endgame<KPsK>(WHITE),   Endgame<KPsK>(BLACK) };
-    Endgame<KPKP>   ScaleKPKP[] = { Endgame<KPKP>(WHITE),   Endgame<KPKP>(BLACK) };
+    int bonus = 0;
 
-    // Helper used to detect a given material distribution
-    bool is_KXK(const Position& pos, Color us) {
-        return  !more_than_one(pos.pieces(~us))
-            && pos.non_pawn_material(us) >= RookValueMg;
-    }
+    // Second-degree polynomial material imbalance, by Tord Romstad
+    for (int pt1 = NO_PIECE_TYPE; pt1 <= QUEEN; ++pt1)
+    {
+        if (!pieceCount[Us][pt1])
+            continue;
 
-    bool is_KBPsK(const Position& pos, Color us) {
-        return   pos.non_pawn_material(us) == BishopValueMg
-            && pos.count<PAWN>(us) >= 1;
-    }
+        int v = QuadraticOurs[pt1][pt1] * pieceCount[Us][pt1];
 
-    bool is_KQKRPs(const Position& pos, Color us) {
-        return  !pos.count<PAWN>(us)
-            && pos.non_pawn_material(us) == QueenValueMg
-            && pos.count<ROOK>(~us) == 1
-            && pos.count<PAWN>(~us) >= 1;
-    }
-
-
-    /// imbalance() calculates the imbalance by comparing the piece count of each
-    /// piece type for both colors.
-
-    template<Color Us>
-    Score imbalance(const int pieceCount[][PIECE_TYPE_NB]) {
-
-        constexpr Color Them = ~Us;
-
-        Score bonus = SCORE_ZERO;
-
-        // Second-degree polynomial material imbalance, by Tord Romstad
-        for (int pt1 = NO_PIECE_TYPE; pt1 <= QUEEN; ++pt1)
-        {
-            if (!pieceCount[Us][pt1])
-                continue;
-
-            int v = QuadraticOurs[pt1][pt1] * pieceCount[Us][pt1];
-
-            for (int pt2 = NO_PIECE_TYPE; pt2 < pt1; ++pt2)
-                v += QuadraticOurs[pt1][pt2] * pieceCount[Us][pt2]
+        for (int pt2 = NO_PIECE_TYPE; pt2 < pt1; ++pt2)
+            v +=  QuadraticOurs[pt1][pt2] * pieceCount[Us][pt2]
                 + QuadraticTheirs[pt1][pt2] * pieceCount[Them][pt2];
 
-            bonus += pieceCount[Us][pt1] * v;
-        }
-
-        return bonus;
+        bonus += pieceCount[Us][pt1] * v;
     }
+
+    return bonus;
+  }
 
 } // namespace
 
 namespace Material {
 
+/// Material::probe() looks up the current position's material configuration in
+/// the material hash table. It returns a pointer to the Entry if the position
+/// is found. Otherwise a new Entry is computed and stored there, so we don't
+/// have to recompute all when the same material configuration occurs again.
 
-    /// Material::probe() looks up the current position's material configuration in
-    /// the material hash table. It returns a pointer to the Entry if the position
-    /// is found. Otherwise a new Entry is computed and stored there, so we don't
-    /// have to recompute all when the same material configuration occurs again.
+Entry* probe(const Position& pos) {
 
-    Entry* probe(const Position& pos) {
+  Key key = pos.material_key();
+  Entry* e = Threads.main()->materialTable[key];
 
-        Key key = pos.material_key();
-        Entry* e = Threads.main()->materialTable[key];
+  if (e->key == key)
+      return e;
 
-        if (e->key == key)
-            return e;
+  std::memset(e, 0, sizeof(Entry));
+  e->key = key;
+  e->factor[WHITE] = e->factor[BLACK] = (uint8_t)SCALE_FACTOR_NORMAL;
 
-        std::memset(e, 0, sizeof(Entry));
-        e->key = key;
-        e->factor[WHITE] = e->factor[BLACK] = (uint8_t)SCALE_FACTOR_NORMAL;
+  Value npm_w = pos.non_pawn_material(WHITE);
+  Value npm_b = pos.non_pawn_material(BLACK);
+  Value npm   = Utility::clamp(npm_w + npm_b, EndgameLimit, MidgameLimit);
 
-        Value npm_w = pos.non_pawn_material(WHITE);
-        Value npm_b = pos.non_pawn_material(BLACK);
-        Value npm = Utility::clamp(npm_w + npm_b, EndgameLimit, MidgameLimit);
+  // Map total non-pawn material into [PHASE_ENDGAME, PHASE_MIDGAME]
+  e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit - EndgameLimit));
 
-        // Map total non-pawn material into [PHASE_ENDGAME, PHASE_MIDGAME]
-        e->gamePhase = Phase(((npm - EndgameLimit) * PHASE_MIDGAME) / (MidgameLimit - EndgameLimit));
+  // Let's look if we have a specialized evaluation function for this particular
+  // material configuration. Firstly we look for a fixed configuration one, then
+  // for a generic one if the previous search failed.
+  if ((e->evaluationFunction = Endgames::probe<Value>(key)) != nullptr)
+      return e;
 
-        // Let's look if we have a specialized evaluation function for this particular
-        // material configuration. Firstly we look for a fixed configuration one, then
-        // for a generic one if the previous search failed.
-        if ((e->evaluationFunction = Endgames::probe<Value>(key)) != nullptr)
-            return e;
+  for (Color c : { WHITE, BLACK })
+      if (is_KXK(pos, c))
+      {
+          e->evaluationFunction = &EvaluateKXK[c];
+          return e;
+      }
 
-        for (Color c : { WHITE, BLACK })
-            if (is_KXK(pos, c))
-            {
-                e->evaluationFunction = &EvaluateKXK[c];
-                return e;
-            }
+  // OK, we didn't find any special evaluation function for the current material
+  // configuration. Is there a suitable specialized scaling function?
+  const auto* sf = Endgames::probe<ScaleFactor>(key);
 
-        // OK, we didn't find any special evaluation function for the current material
-        // configuration. Is there a suitable specialized scaling function?
-        const auto* sf = Endgames::probe<ScaleFactor>(key);
+  if (sf)
+  {
+      e->scalingFunction[sf->strongSide] = sf; // Only strong color assigned
+      return e;
+  }
 
-        if (sf)
-        {
-            e->scalingFunction[sf->strongSide] = sf; // Only strong color assigned
-            return e;
-        }
+  // We didn't find any specialized scaling function, so fall back on generic
+  // ones that refer to more than one material distribution. Note that in this
+  // case we don't return after setting the function.
+  for (Color c : { WHITE, BLACK })
+  {
+    if (is_KBPsK(pos, c))
+        e->scalingFunction[c] = &ScaleKBPsK[c];
 
-        // We didn't find any specialized scaling function, so fall back on generic
-        // ones that refer to more than one material distribution. Note that in this
-        // case we don't return after setting the function.
-        for (Color c : { WHITE, BLACK })
-        {
-            if (is_KBPsK(pos, c))
-                e->scalingFunction[c] = &ScaleKBPsK[c];
+    else if (is_KQKRPs(pos, c))
+        e->scalingFunction[c] = &ScaleKQKRPs[c];
+  }
 
-            else if (is_KQKRPs(pos, c))
-                e->scalingFunction[c] = &ScaleKQKRPs[c];
-        }
+  if (npm_w + npm_b == VALUE_ZERO && pos.pieces(PAWN)) // Only pawns on the board
+  {
+      if (!pos.count<PAWN>(BLACK))
+      {
+          assert(pos.count<PAWN>(WHITE) >= 2);
 
-        if (npm_w + npm_b == VALUE_ZERO && pos.pieces(PAWN)) // Only pawns on the board
-        {
-            if (!pos.count<PAWN>(BLACK))
-            {
-                assert(pos.count<PAWN>(WHITE) >= 2);
+          e->scalingFunction[WHITE] = &ScaleKPsK[WHITE];
+      }
+      else if (!pos.count<PAWN>(WHITE))
+      {
+          assert(pos.count<PAWN>(BLACK) >= 2);
 
-                e->scalingFunction[WHITE] = &ScaleKPsK[WHITE];
-            }
-            else if (!pos.count<PAWN>(WHITE))
-            {
-                assert(pos.count<PAWN>(BLACK) >= 2);
+          e->scalingFunction[BLACK] = &ScaleKPsK[BLACK];
+      }
+      else if (pos.count<PAWN>(WHITE) == 1 && pos.count<PAWN>(BLACK) == 1)
+      {
+          // This is a special case because we set scaling functions
+          // for both colors instead of only one.
+          e->scalingFunction[WHITE] = &ScaleKPKP[WHITE];
+          e->scalingFunction[BLACK] = &ScaleKPKP[BLACK];
+      }
+  }
 
-                e->scalingFunction[BLACK] = &ScaleKPsK[BLACK];
-            }
-            else if (pos.count<PAWN>(WHITE) == 1 && pos.count<PAWN>(BLACK) == 1)
-            {
-                // This is a special case because we set scaling functions
-                // for both colors instead of only one.
-                e->scalingFunction[WHITE] = &ScaleKPKP[WHITE];
-                e->scalingFunction[BLACK] = &ScaleKPKP[BLACK];
-            }
-        }
+  // Zero or just one pawn makes it difficult to win, even with a small material
+  // advantage. This catches some trivial draws like KK, KBK and KNK and gives a
+  // drawish scale factor for cases such as KRKBP and KmmKm (except for KBBKN).
+  if (!pos.count<PAWN>(WHITE) && npm_w - npm_b <= BishopValueMg)
+      e->factor[WHITE] = uint8_t(npm_w <  RookValueMg   ? SCALE_FACTOR_DRAW :
+                                 npm_b <= BishopValueMg ? 4 : 14);
 
-        // Zero or just one pawn makes it difficult to win, even with a small material
-        // advantage. This catches some trivial draws like KK, KBK and KNK and gives a
-        // drawish scale factor for cases such as KRKBP and KmmKm (except for KBBKN).
-        if (!pos.count<PAWN>(WHITE) && npm_w - npm_b <= BishopValueMg)
-            e->factor[WHITE] = uint8_t(npm_w < RookValueMg ? SCALE_FACTOR_DRAW :
-                npm_b <= BishopValueMg ? 4 : 14);
+  if (!pos.count<PAWN>(BLACK) && npm_b - npm_w <= BishopValueMg)
+      e->factor[BLACK] = uint8_t(npm_b <  RookValueMg   ? SCALE_FACTOR_DRAW :
+                                 npm_w <= BishopValueMg ? 4 : 14);
 
-        if (!pos.count<PAWN>(BLACK) && npm_b - npm_w <= BishopValueMg)
-            e->factor[BLACK] = uint8_t(npm_b < RookValueMg ? SCALE_FACTOR_DRAW :
-                npm_w <= BishopValueMg ? 4 : 14);
+  // Evaluate the material imbalance. We use PIECE_TYPE_NONE as a place holder
+  // for the bishop pair "extended piece", which allows us to be more flexible
+  // in defining bishop pair bonuses.
+  const int pieceCount[COLOR_NB][PIECE_TYPE_NB] = {
+  { pos.count<BISHOP>(WHITE) > 1, pos.count<PAWN>(WHITE), pos.count<KNIGHT>(WHITE),
+    pos.count<BISHOP>(WHITE)    , pos.count<ROOK>(WHITE), pos.count<QUEEN >(WHITE) },
+  { pos.count<BISHOP>(BLACK) > 1, pos.count<PAWN>(BLACK), pos.count<KNIGHT>(BLACK),
+    pos.count<BISHOP>(BLACK)    , pos.count<ROOK>(BLACK), pos.count<QUEEN >(BLACK) } };
 
-        // Evaluate the material imbalance. We use PIECE_TYPE_NONE as a place holder
-        // for the bishop pair "extended piece", which allows us to be more flexible
-        // in defining bishop pair bonuses.
-        const int pieceCount[COLOR_NB][PIECE_TYPE_NB] = {
-        { pos.count<BISHOP>(WHITE) > 1, pos.count<PAWN>(WHITE), pos.count<KNIGHT>(WHITE),
-            pos.count<BISHOP>(WHITE)    , pos.count<ROOK>(WHITE), pos.count<QUEEN >(WHITE) },
-        { pos.count<BISHOP>(BLACK) > 1, pos.count<PAWN>(BLACK), pos.count<KNIGHT>(BLACK),
-            pos.count<BISHOP>(BLACK)    , pos.count<ROOK>(BLACK), pos.count<QUEEN >(BLACK) } };
-
-        e->score = (imbalance<WHITE>(pieceCount) - imbalance<BLACK>(pieceCount)) / 16;
-        return e;
-    }
+  e->value = int16_t((imbalance<WHITE>(pieceCount) - imbalance<BLACK>(pieceCount)) / 16);
+  return e;
+}
 
 } // namespace Material
