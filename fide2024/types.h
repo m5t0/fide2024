9bfdabf4e9587fc4f3a1341c0aea37b30a7667da
moto <mo_to1015@yahoo.co.jp>
2025-01-21 18:01:13 +0900

    stockfish140

--- fide2024/types.h
+++ fide2024/types.h
@@ -102,7 +102,7 @@ typedef uint64_t Key;
 typedef uint64_t Bitboard;
 
 constexpr int MAX_MOVES = 256;
-constexpr int MAX_PLY   = 246;
+constexpr int MAX_PLY   = 80;
 
 /// A move needs 16 bits to be stored
 ///
